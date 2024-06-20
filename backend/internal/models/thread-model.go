a1753cc0888feeb04fbf87d756b372591b16417b
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-06-20 18:09:34 +0600

    Make community ID nullable in Thread model

--- backend/internal/models/thread-model.go
+++ backend/internal/models/thread-model.go
@@ -4,7 +4,7 @@ type Thread struct {
 	BaseModel
 	Text        string `json:"text"`
 	UserID      uint   `json:"user_id"`
-	CommunityId uint   `json:"community_id" gorm:"column:community_id;not null"`
+	CommunityId uint   `json:"community_id" gorm:"column:community_id;null"`
 	ParentId    *uint  `json:"parent_id" gorm:"column:parent_id,index"` //Adding an index to the ParentID field (gorm:"index") can improve query performance when retrieving child threads.
 
 	Children []Thread `json:"children" gorm:"foreignKey:ParentId"`
