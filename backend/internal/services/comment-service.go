a1bfa6022ab2f0d0f9c50a7f2d71a431876c1ece
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-07-03 22:45:45 +0600

    Comment service with create comment method

--- backend/internal/services/comment-service.go
+++ backend/internal/services/comment-service.go
@@ -1,1 +1,22 @@
 package services
+
+import (
+	"github.com/manjurulhoque/threadly/backend/internal/models"
+	"github.com/manjurulhoque/threadly/backend/internal/repositories"
+)
+
+type CommentService interface {
+	CreateComment(comment *models.Comment) error
+}
+
+type commentService struct {
+	commentRepo repositories.CommentRepository
+}
+
+func NewCommentService(commentRepo repositories.CommentRepository) CommentService {
+	return &commentService{commentRepo}
+}
+
+func (s *commentService) CreateComment(comment *models.Comment) error {
+	return s.commentRepo.CreateComment(comment)
+}
