85cf1fc7033ce4b523b71a7060f747fb6628f80c
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-07-04 13:55:45 +0600

    Comment handler to create new comment with thread ID and validation

--- backend/internal/handlers/comment-handler.go
+++ backend/internal/handlers/comment-handler.go
@@ -1,1 +1,48 @@
 package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/manjurulhoque/threadly/backend/internal/models"
+	"github.com/manjurulhoque/threadly/backend/internal/services"
+	"net/http"
+)
+
+type CommentHandler struct {
+	commentService services.CommentService
+}
+
+func NewCommentHandler(commentService services.CommentService) *CommentHandler {
+	return &CommentHandler{commentService: commentService}
+}
+
+// CreateComment Create comment handler
+func (h *CommentHandler) CreateComment(c *gin.Context) {
+	userId, exists := c.Get("userId")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	var input struct {
+		Content  string `json:"content" validate:"required"`
+		ThreadId uint   `json:"thread_id" validate:"required"`
+	}
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	comment := models.Comment{
+		Content:  input.Content,
+		ThreadId: input.ThreadId,
+		UserId:   userId.(uint),
+	}
+
+	err := h.commentService.CreateComment(&comment)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"comment": comment})
+}
--- backend/internal/models/comment-model.go
+++ backend/internal/models/comment-model.go
@@ -3,8 +3,8 @@ package models
 type Comment struct {
 	BaseModel
 	Content  string `json:"content" gorm:"not null"`
-	UserID   uint   `json:"user_id" gorm:"not null"`
-	ThreadID uint   `json:"thread_id" gorm:"not null;index"`
+	UserId   uint   `json:"user_id" gorm:"not null"`
+	ThreadId uint   `json:"thread_id" gorm:"not null;index"`
 }
 
 func (c *Comment) TableName() string {
