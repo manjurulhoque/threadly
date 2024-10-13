fd6407420694fadc1a7d94dbeac7d3319838f0d0
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-10-13 14:10:50 +0600

    Use sync.Once to init DB once

--- backend/internal/db/db.go
+++ backend/internal/db/db.go
@@ -7,24 +7,39 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"log/slog"
+	"sync"
 )
 
-var DB *gorm.DB
+var (
+	DB     *gorm.DB
+	dbOnce sync.Once
+)
 
 // InitializeDB creates a new GORM DB connection
 func InitializeDB() (*gorm.DB, error) {
-	conf := config.GetDBConfig()
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		conf.Host, conf.User, conf.Password, conf.DBName, conf.Port, conf.SSLMode)
+	var err error
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+	// Use sync.Once to initialize the database connection only once
+	dbOnce.Do(func() {
+		conf := config.GetDBConfig()
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+			conf.Host, conf.User, conf.Password, conf.DBName, conf.Port, conf.SSLMode)
+
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+		if err != nil {
+			slog.Error("Failed to connect to database", "error", err.Error())
+		} else {
+			slog.Info("Database connected successfully")
+		}
 	})
-	if err != nil {
+
+	if DB == nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
-	DB = db
-	return db, nil
+
+	return DB, nil
 }
 
 // CloseDB closes the GORM DB connection
