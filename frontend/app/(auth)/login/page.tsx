4af6ccad820053231e36fc008f023cd558cdc0b1
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-11-01 21:55:42 +0600

    Login and register page design

---
+++ frontend/app/(auth)/login/page.tsx
@@ -0,0 +1,14 @@
+import Login from "@/components/auth/Login";
+
+export const metadata = {
+    title: "Login",
+    description: "Login to your account",
+}
+
+const Page = () => {
+    return (
+        <Login/>
+    );
+}
+
+export default Page;
---
+++ frontend/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import Register from "@/components/auth/Register";
+
+export const metadata = {
+    title: "Register",
+    description: "Register for an account",
+}
+
+const Page = () => {
+    return <Register/>
+}
+
+export default Page;
