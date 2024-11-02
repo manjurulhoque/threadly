fc56f84d32743e516f98352fb4d940e25e18b9f2
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-11-02 23:45:59 +0600

    Register implemented

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -1,11 +1,20 @@
 import Register from "@/components/auth/Register";
+import { permanentRedirect } from "next/navigation";
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
+import { UserSession } from "@/types/user-session.type";
 
 export const metadata = {
     title: "Register",
     description: "Register for an account",
 }
 
-const Page = () => {
+const Page = async () => {
+    const data: UserSession | null = await getServerSession(authOptions);
+    if (data?.user) {
+        permanentRedirect('/');
+    }
+
     return <Register/>
 }
 
--- frontend/components/auth/Register.tsx
+++ frontend/components/auth/Register.tsx
@@ -2,9 +2,47 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const Register = () => {
     const [isClient, setIsClient] = useState(false);
+    const [message, setMessage] = useState("");
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: {errors},
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        setMessage("");
+        try {
+            const response = await fetch('/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({name: data.name, email: data.email, password: data.password}),
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                setMessage(data.message || 'Something went wrong');
+            } else {
+                const data = await response.json();
+                setMessage('');
+                toast.success("Signup was successful");
+                setTimeout(() => {
+                    window.location.reload();
+                }, 500);
+            }
+        } catch (error) {
+            console.error(error);
+            toast.error('An unexpected error occurred');
+        }
+    }
 
     useEffect(() => {
         setIsClient(true);
@@ -27,13 +65,14 @@ const Register = () => {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" method="POST" className="space-y-6">
+                    <form onSubmit={handleSubmit(onSubmit)} method="POST" className="space-y-6">
                         <div>
                             <label htmlFor="name" className="block text-sm/6 font-medium text-white">
                                 Name
                             </label>
                             <div className="mt-2">
                                 <input
+                                    {...register("name", {required: true})}
                                     id="name"
                                     name="name"
                                     type="text"
@@ -50,6 +89,7 @@ const Register = () => {
                             </label>
                             <div className="mt-2">
                                 <input
+                                    {...register("email", {required: true})}
                                     id="email"
                                     name="email"
                                     type="email"
@@ -68,6 +108,7 @@ const Register = () => {
                             </div>
                             <div className="mt-2">
                                 <input
+                                    {...register("password", {required: true})}
                                     id="password"
                                     name="password"
                                     type="password"
@@ -77,13 +118,15 @@ const Register = () => {
                                 />
                             </div>
                         </div>
-
+                        {message && (
+                            <p className="text-red-500 text-xs italic mb-3">{message}</p>
+                        )}
                         <div>
                             <button
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                             >
-                                Sign in
+                                Register
                             </button>
                         </div>
                     </form>
