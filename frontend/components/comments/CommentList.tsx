5cfc7b90761afa2aae90661428cd01a6cbcad1a4
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-11-11 00:08:54 +0600

    Fetch list of comments for thread id using redux toolkit

--- frontend/components/comments/CommentList.tsx
+++ frontend/components/comments/CommentList.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+import { useGetCommentsByThreadIdQuery } from "@/store/comments/commentApi";
+
 const CommentList = ({threadId}: {threadId: number}) => {
+    const { data, error, isLoading } = useGetCommentsByThreadIdQuery(threadId);
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    const { comments } = data;
+
     return (
         <div>
 
