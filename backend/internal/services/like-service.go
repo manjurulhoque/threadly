55641f0b3d89cee2030f9cd664346da7e4874fd0
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-11-14 23:48:42 +0600

    LikeThread interface method added to like service

--- backend/internal/services/like-service.go
+++ backend/internal/services/like-service.go
@@ -1,11 +1,22 @@
 package services
 
-import "github.com/manjurulhoque/threadly/backend/internal/repositories"
+import (
+	"github.com/manjurulhoque/threadly/backend/internal/models"
+	"github.com/manjurulhoque/threadly/backend/internal/repositories"
+)
 
 type LikeService interface {
-	LikeThread(threadId uint, userId uint) error
+	LikeThread(like *models.Like) error
 }
 
 type likeService struct {
 	likeRepo repositories.LikeRepository
+}
+
+func NewLikeService(repo repositories.LikeRepository) LikeService {
+	return &likeService{likeRepo: repo}
+}
+
+func (s *likeService) LikeThread(like *models.Like) error {
+	return s.likeRepo.LikeThread(like)
 }
