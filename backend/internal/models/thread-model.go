512194534972c8df46e9ce625b58dac23ec3ffea
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-11-14 23:32:19 +0600

    Added like_count column for efficiency

--- backend/internal/models/thread-model.go
+++ backend/internal/models/thread-model.go
@@ -6,7 +6,8 @@ type Thread struct {
 	UserID      uint   `json:"user_id" gorm:"column:user_id;not null"`
 	CommunityId uint   `json:"community_id" gorm:"column:community_id;null"`
 
-	User ThreadUser `json:"user" gorm:"foreignKey:UserID"`
+	User      ThreadUser `json:"user" gorm:"foreignKey:UserID"`
+	LikeCount int        `json:"like_count" gorm:"default:0"` // Add like count for efficiency
 }
 
 // TableName overrides the table name
