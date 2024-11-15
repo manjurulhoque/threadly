e6ef0e4c441df49abe9aee75c0479b3fd7051bcf
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-11-15 12:03:02 +0600

    feat: Add likeThread mutation to likeApi for liking threads

---
+++ frontend/store/likes/likeApi.ts
@@ -0,0 +1,22 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import DynamicBaseQuery from "@/store/dynamic-base-query";
+
+export const likeApi = createApi({
+    reducerPath: 'likeApi',
+    baseQuery: DynamicBaseQuery,
+    tagTypes: [ 'Like' ],
+    endpoints: (builder) => ({
+        likeThread: builder.mutation({
+            query: (threadId: number) => ({
+                url: `threads/${threadId}/like`,
+                method: 'POST',
+            }),
+            invalidatesTags: [ 'Like' ],
+        }),
+    }),
+});
+
+export const { useLikeThreadMutation } = likeApi;
+
+export default likeApi;
+
