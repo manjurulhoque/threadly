df638d5a134cdc02969762b478cd6746c0aa9d38
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2024-11-16 12:04:24 +0600

    Service method to get threads where user was mentioned

--- backend/internal/services/thread-service.go
+++ backend/internal/services/thread-service.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"fmt"
 	"github.com/manjurulhoque/threadly/backend/internal/models"
 	"github.com/manjurulhoque/threadly/backend/internal/repositories"
+	"log/slog"
 )
 
 type ThreadService interface {
@@ -11,6 +13,7 @@ type ThreadService interface {
 	GetThreadById(threadId uint) (*models.Thread, error)
 	TotalThreadsByUser(userId uint) (int64, error)
 	GetThreadsUserReplied(userId uint) ([]models.ThreadWithLike, error)
+	GetThreadsWhereUserWasMentioned(userId uint) ([]models.ThreadWithLike, error)
 }
 
 type threadService struct {
@@ -22,7 +25,11 @@ func NewThreadService(repo repositories.ThreadRepository) ThreadService {
 }
 
 func (s *threadService) CreateThread(thread *models.Thread) error {
-	return s.threadRepo.CreateThread(thread)
+	err := s.threadRepo.CreateThread(thread)
+	if err != nil {
+		slog.Error("Failed to create thread", "error", err.Error())
+	}
+	return err
 }
 
 func (s *threadService) GetThreadsForUser(userId uint) ([]models.ThreadWithLike, error) {
@@ -30,7 +37,11 @@ func (s *threadService) GetThreadsForUser(userId uint) ([]models.ThreadWithLike,
 }
 
 func (s *threadService) GetThreadById(threadId uint) (*models.Thread, error) {
-	return s.threadRepo.GetThreadById(threadId)
+	thread, err := s.threadRepo.GetThreadById(threadId)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get thread by id %d: %w", threadId, err)
+	}
+	return thread, nil
 }
 
 func (s *threadService) TotalThreadsByUser(userId uint) (int64, error) {
@@ -39,4 +50,13 @@ func (s *threadService) TotalThreadsByUser(userId uint) (int64, error) {
 
 func (s *threadService) GetThreadsUserReplied(userId uint) ([]models.ThreadWithLike, error) {
 	return s.threadRepo.GetThreadsUserReplied(userId)
+}
+
+func (s *threadService) GetThreadsWhereUserWasMentioned(userId uint) ([]models.ThreadWithLike, error) {
+	threads, err := s.threadRepo.GetThreadsWhereUserWasMentioned(userId)
+	if err != nil {
+		slog.Error("Failed to get threads where user was mentioned", "error", err.Error())
+		return nil, fmt.Errorf("failed to get threads where user was mentioned: %w", err)
+	}
+	return threads, nil
 }
