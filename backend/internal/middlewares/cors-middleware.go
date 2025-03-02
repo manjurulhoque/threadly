4cc85cba14fea2f1715a1f15eceb738762c0a157
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-03-03 01:27:18 +0600

    update CORS

--- backend/internal/middlewares/cors-middleware.go
+++ backend/internal/middlewares/cors-middleware.go
@@ -4,22 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-		// origin := c.Request.Header.Get("Origin")
-		// referer := c.Request.Header.Get("Referer")
-
-		// if origin == "" && referer != "" {
-		// 	origin = referer
-		// }
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        // Allow frontend requests
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://threadly.manjurulhoque.com")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
+        // Handle preflight requests
         if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
+            c.Writer.WriteHeader(200)
             return
         }
 
